2f8d000826e00a1ddb642a44ad24bbf686d3b45b
Fernando <luisfernandosalasg@gmail.com>
2022-04-12 05:38:30 -0500

    Sum of two lowest positive integers

---
+++ 7kyu/Sum of two lowest positive integers.js
@@ -0,0 +1,7 @@
+/* https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript */
+function sumTwoSmallestNumbers(numbers) {
+	numbers.sort((a, b) => a - b);
+	return numbers[0] + numbers[1];
+}
+
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]));
